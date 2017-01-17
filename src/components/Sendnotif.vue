b637b1e5eed4964d8b7c21bd4a32fe03152706d9
Loic Bertaux <loic.bertaux@ensta.fr>
2017-01-17 17:26:14 +0100

    Adds a basic route structure with static routes

---
+++ src/components/Sendnotif.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="send-notif">
+    <h1>{{ msg }}</h1>
+    <router-link to="/home">Voir mes annonces</router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'send-notif',
+    data () {
+      return {
+        msg: 'Envoyer une notification'
+      }
+    }
+  }
+</script>
