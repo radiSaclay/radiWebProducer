3ed93be0ee993e43df71ef5a5a5f91fe820ead11
Tiberio <tiberiusferreira@gmail.com>
2017-01-30 04:34:15 +0100

    Make changes so it can be deployed to server

---
+++ server.js
@@ -0,0 +1,12 @@
+/**
+ * Created by tiberio on 30/01/2017.
+ */
+
+var express = require('express')
+var path = require('path');
+var serveStatic = require('serve-static')
+app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started '+port);
