0776dcea4938a5b4ffd97404527a0ff04f1a334e
Tiberio <tiberiusferreira@gmail.com>
2017-02-02 16:32:39 +0100

    Send updates to server and support Name/Address/Email/Phone

--- src/routes/FarmHome.vue
+++ src/routes/FarmHome.vue
@@ -1,12 +1,20 @@
 <template xmlns:v-on="http://www.w3.org/1999/xhtml" xmlns:v-bind="http://www.w3.org/1999/xhtml">
   <div class="farmhome">
     <div class = "nameline">
-    <input  :disabled="!editingName" v-model="name" placeholder="">
-    <button v-on:click="editName">{{editingName ? "Done" : "Edit"}}</button>
+      <input  :disabled="!editingName" v-model="name" placeholder="">
+      <button v-on:click="editName">{{editingName ? "Done" : "Edit"}}</button>
     </div>
     <div class = "addressline">
-    <input  :disabled="!editingAddress" v-model="address" placeholder="">
-    <button v-on:click="editAddress">{{editingAddress ? "Done" : "Edit"}}</button>
+      <input  :disabled="!editingAddress" v-model="address" placeholder="">
+      <button v-on:click="editAddress">{{editingAddress ? "Done" : "Edit"}}</button>
+    </div>
+    <div class = "emailline">
+      <input  :disabled="!editingEmail" v-model="email" placeholder="">
+      <button v-on:click="editEmail">{{editingEmail ? "Done" : "Edit"}}</button>
+    </div>
+    <div class = "phoneline">
+      <input  :disabled="!editingPhone" v-model="phone" placeholder="">
+      <button v-on:click="editPhone">{{editingPhone ? "Done" : "Edit"}}</button>
     </div>
   </div>
 
@@ -15,19 +23,28 @@
 
 
 <script>
+  let farmid
   export default {
+    /* global localStorage:true */
     data () {
-      this.$http.get('http://ec2-52-56-114-123.eu-west-2.compute.amazonaws.com/api/farms/1').then(function successCallback (response) {
-        // this callback will be called asynchronously
-        // when the response is available
-        this.name = response.body['name']
-        this.address = response.body['address']
-        this.email = response.body['email']
-        this.phone = response.body['phone']
-      }, function errorCallback (response) {
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
-        console.log(response)
+      this.$http.get('http://ec2-52-56-114-123.eu-west-2.compute.amazonaws.com/auth/user', {
+        headers: {
+          Authorization: localStorage.getItem('id_token')
+        }
+      }).then(function successCallback (response) {
+        farmid = response.body['farm']['id']
+        this.$http.get('http://ec2-52-56-114-123.eu-west-2.compute.amazonaws.com/api/farms/' + farmid).then(function successCallback (response) {
+          // this callback will be called asynchronously
+          // when the response is available
+          this.name = response.body['name']
+          this.address = response.body['address']
+          this.email = response.body['email']
+          this.phone = response.body['phone']
+        }, function errorCallback (response) {
+          // called asynchronously if an error occurs
+          // or server returns response with an error status.
+          console.log(response)
+        })
       })
       return {
         name: '',
@@ -36,15 +53,59 @@
         phone: '',
         show: false,
         editingName: false,
-        editingAddress: false
+        editingAddress: false,
+        editingEmail: false,
+        editingPhone: false
       }
     },
     methods: {
       editName: function () {
+        if (this.editingName) {
+          this.$http({url: 'http://ec2-52-56-114-123.eu-west-2.compute.amazonaws.com/api/farms/' + farmid, method: 'PUT',
+            headers: {
+              'Authorization': localStorage.getItem('id_token'), 'Content-Type': 'application/json'
+            }, body: {
+              "name": this.name
+            }
+          })
+        }
         this.editingName = !this.editingName
       },
       editAddress: function () {
+        if (this.editingAddress) {
+          this.$http({url: 'http://ec2-52-56-114-123.eu-west-2.compute.amazonaws.com/api/farms/' + farmid, method: 'PUT',
+            headers: {
+              'Authorization': localStorage.getItem('id_token'), 'Content-Type': 'application/json'
+            }, body: {
+              "address": this.address
+            }
+          })
+        }
         this.editingAddress = !this.editingAddress
+      },
+      editEmail: function () {
+        if (this.editingEmail) {
+          this.$http({url: 'http://ec2-52-56-114-123.eu-west-2.compute.amazonaws.com/api/farms/' + farmid, method: 'PUT',
+            headers: {
+              'Authorization': localStorage.getItem('id_token'), 'Content-Type': 'application/json'
+            }, body: {
+              "email": this.email
+            }
+          })
+        }
+        this.editingEmail = !this.editingEmail
+      },
+      editPhone: function () {
+        if (this.editingPhone) {
+          this.$http({url: 'http://ec2-52-56-114-123.eu-west-2.compute.amazonaws.com/api/farms/' + farmid, method: 'PUT',
+            headers: {
+              'Authorization': localStorage.getItem('id_token'), 'Content-Type': 'application/json'
+            }, body: {
+              "phone": this.phone
+            }
+          })
+        }
+        this.editingPhone = !this.editingPhone
       }
     }
   }
