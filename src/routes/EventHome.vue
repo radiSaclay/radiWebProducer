17ae5256682551332ce961d41b7d165b05de65f2
Tiberio <tiberiusferreira@gmail.com>
2017-02-10 02:36:38 +0100

    change date format to DD/MM/YY HH/MM

--- src/routes/EventHome.vue
+++ src/routes/EventHome.vue
@@ -1,4 +1,4 @@
-<template>
+<template xmlns:v-bind="http://www.w3.org/1999/xhtml">
   <div class="eventhome">
     <div v-for="(event, event_index) in events" :key="event.id">
       <h1> Event {{event.id}} </h1>
@@ -10,11 +10,11 @@
       </div>
       <div>
         <h4>Date de Début</h4>
-        <Flatpickr :disabled="!editingEvent[event.id]" v-model="event.beginAt" :options="fpOptionsBeginDate[event_index]"/>
+        <Flatpickr :disabled="!editingEvent[event.id]" v-model="event.beginAt" :options="fpOptionsBeginDate[event_index]"></Flatpickr>
       </div>
       <div>
         <h4>Date de fin</h4>
-        <Flatpickr :disabled="!editingEvent[event.id]" v-model="event.endAt" :options="fpOptionsEndDate[event_index]"/>
+        <Flatpickr :disabled="!editingEvent[event.id]" v-model="event.endAt" :options="fpOptionsEndDate[event_index]"></Flatpickr>
       </div>
       <h3> Produits </h3>
       <div v-for="(event_product, index) in event.products">
@@ -46,7 +46,6 @@
   import VueFlatpickr from 'vue-flatpickr'
   import 'vue-flatpickr/theme/airbnb.css'
   Vue.use(VueFlatpickr)
-
   function getProductsID(products){
     let productsID = new Array()
     products.forEach(function (product){
@@ -84,11 +83,15 @@
         fpOptionsBeginDate[index] = {
           allowInput: true,
             enableTime: true,
+            dateFormat: 'd/m/y H:i',
+            time_24hr: true,
             defaultDate: event.beginAt*1000
         }
         fpOptionsEndDate[index] = {
           allowInput: true,
           enableTime: true,
+          dateFormat: 'd/m/y H:i',
+          time_24hr: true,
           defaultDate: event.endAt*1000
         }
       })
@@ -142,7 +145,9 @@
       }
     },
     methods: {
+
       editEvent: function (event) {
+
         let vue_event_id
         vue_event_id = findVueId(this.events, event.target.id)
         if (this.editingEvent) {
@@ -152,8 +157,8 @@
             }, body: {
               "title": this.events[vue_event_id].title,
               "description": this.events[vue_event_id].description,
-              "beginAt": moment(this.events[vue_event_id].beginAt).unix(),
-              "endAt": moment(this.events[vue_event_id].endAt).unix(),
+              "beginAt": moment(this.events[vue_event_id].beginAt, "DD/MM/YY HH:mm").unix(),
+              "endAt": moment(this.events[vue_event_id].endAt, "DD/MM/YY HH:mm").unix(),
               "products": getProductsID(this.events[vue_event_id].products)
             }
           })
