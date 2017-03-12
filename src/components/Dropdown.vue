7430f19c12188a632bd4866c129f360cd3e564de
Lea Lincker <lea.lincker@gmail.com>
2017-03-12 10:53:08 +0100

    Adds indentation changes

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -23,101 +23,100 @@
       </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
-import Vue from 'vue';
-export default {
-  data() {
-    return {
-      open: false,
-      current: 0
-    }
-  },
-  props: {
-    suggestions: {
-      type: Array,
-      required: true
-    },
-    selection: {
-      type: String,
-      required: true,
-      twoWay: true
-    },
-    isDisabled: {
-      type: Boolean,
-      required: false
-    },
-    product_index: {
-      type: Number,
-      required: true
-    },
-    event_index: {
-      type: Number,
-      required: true
-    }
-  },
-  computed: {
-    matches() {
-      return this.suggestions.filter(({name: str}) => {
-        return str.indexOf(this.selection) >= 0;
-      });
-    },
-    openSuggestion() {
-      if (this.selection !== "" &&
-      this.matches.length != 0 &&
-      this.open === true){
-        return "open";
+  import Vue from 'vue';
+  export default {
+    data() {
+      return {
+        open: false,
+        current: 0
       }
+    },
+    props: {
+      suggestions: {
+        type: Array,
+        required: true
+      },
+      selection: {
+        type: String,
+        required: true,
+        twoWay: true
+      },
+      isDisabled: {
+        type: Boolean,
+        required: false
+      },
+      product_index: {
+        type: Number,
+        required: true
+      },
+      event_index: {
+        type: Number,
+        required: true
+      }
+    },
+    computed: {
+      matches() {
+        return this.suggestions.filter(({name: str}) => {
+          return str.indexOf(this.selection) >= 0;
+        });
+      },
+      openSuggestion() {
+        if (this.selection !== "" &&
+        this.matches.length != 0 &&
+        this.open === true){
+          return "open";
+        }
 
-    }
-  },
-  methods: {
-    //When enter pressed on the input
-    enter(event_index,product_index) {
-      var element = this.matches[this.current];
-      this.selection = this.matches[this.current].name;
-      this.open = false;
-      this.$parent.editProduct(event_index,product_index,element);
-    },
-    //When up pressed while suggestions are open
-    up() {
-      if(this.current > 0)
-      this.current--;
-    },
-    //When down pressed while suggestions are open
-    down() {
-      if(this.current < this.suggestions.length - 1)
-      this.current++;
-    },
-    //For highlighting element
-    isActive(index) {
-      if (index === this.current){
-        return "active";
       }
     },
-    //When the user changes input
-    change() {
-      if (this.open == false) {
+    methods: {
+      //When enter pressed on the input
+      enter(event_index,product_index) {
+        var element = this.matches[this.current];
+        this.selection = this.matches[this.current].name;
+        this.open = false;
+        this.$parent.editProduct(event_index,product_index,element);
+      },
+      //When up pressed while suggestions are open
+      up() {
+        if(this.current > 0)
+        this.current--;
+      },
+      //When down pressed while suggestions are open
+      down() {
+        if(this.current < this.suggestions.length - 1)
+        this.current++;
+      },
+      //For highlighting element
+      isActive(index) {
+        if (index === this.current){
+          return "active";
+        }
+      },
+      //When the user changes input
+      change() {
+        if (this.open == false) {
+          this.open = true;
+          this.current = 0;
+        }
+      },
+      //When one of the suggestion is clicked
+      suggestionClick(event_index,product_index,index) {
+        var element = this.matches[index];
+        this.selection = this.matches[index].name;
+        this.open = false;
+        this.$parent.editProduct(event_index,product_index,element);
+      },
+      //Removes the previous selected value when the input is clicked.
+      //So that you don't have to delete everything by hand in order to see all the others previous products
+      removeSelection() {
+        this.selection="";
+        this.current=0;
         this.open = true;
-        this.current = 0;
       }
-    },
-    //When one of the suggestion is clicked
-    suggestionClick(event_index,product_index,index) {
-      var element = this.matches[index];
-      this.selection = this.matches[index].name;
-      this.open = false;
-      this.$parent.editProduct(event_index,product_index,element);
-    },
-    //Removes the previous selected value when the input is clicked.
-    //So that you don't have to delete everything by hand in order to see all the others previous products
-    removeSelection() {
-      this.selection="";
-      this.current=0;
-      this.open = true;
     }
   }
-}
 </script>
--- src/routes/EventHome.vue
+++ src/routes/EventHome.vue
@@ -89,134 +89,134 @@
 </template>
 
 <script>
-import Vue from 'vue'
-// Moment deals with converting dates between UNIX timestamp and human readable format
-import moment from 'moment'
-import VueFlatpickr from 'vue-flatpickr'
-import 'vue-flatpickr/theme/airbnb.css'
-import * as api from '../api/general'
-import settings from '../config/settings'
-import Dropdown from '../components/Dropdown'
-Vue.use(VueFlatpickr)
-let farm_id, events, farm_products
+  import Vue from 'vue'
+  // Moment deals with converting dates between UNIX timestamp and human readable format
+  import moment from 'moment'
+  import VueFlatpickr from 'vue-flatpickr'
+  import 'vue-flatpickr/theme/airbnb.css'
+  import * as api from '../api/general'
+  import settings from '../config/settings'
+  import Dropdown from '../components/Dropdown'
+  Vue.use(VueFlatpickr)
+  let farm_id, events, farm_products
 
-// Receives a list of events and creates a field for each one with the options used by the DatePicker
-// Also converts the date to human readable format
-function setupDatePickerOption(event){
-  event.beginAt = moment(event.beginAt*1000).format('DD/MM/YY HH:mm')
-  event.endAt = moment(event.endAt*1000).format('DD/MM/YY HH:mm')
-  // It is done on a per event basis to have more flexibility in the future
-  // Such as allow the alternative display format (altInput) to decouple internal representation from shown
-  // representation. However this breaks the :disable flag in HTML and couldn't yet find a way to disable input
-  // using the DatePicker API.
-  event.fpOptionsBeginDate = {
-    allowInput: false,
-    enableTime: true,
-    dateFormat: 'd/m/y H:i',
-    time_24hr: true
+  // Receives a list of events and creates a field for each one with the options used by the DatePicker
+  // Also converts the date to human readable format
+  function setupDatePickerOption(event){
+    event.beginAt = moment(event.beginAt*1000).format('DD/MM/YY HH:mm')
+    event.endAt = moment(event.endAt*1000).format('DD/MM/YY HH:mm')
+    // It is done on a per event basis to have more flexibility in the future
+    // Such as allow the alternative display format (altInput) to decouple internal representation from shown
+    // representation. However this breaks the :disable flag in HTML and couldn't yet find a way to disable input
+    // using the DatePicker API.
+    event.fpOptionsBeginDate = {
+      allowInput: false,
+      enableTime: true,
+      dateFormat: 'd/m/y H:i',
+      time_24hr: true
+    }
+    event.fpOptionsEndDate = {
+      allowInput: false,
+      enableTime: true,
+      dateFormat: 'd/m/y H:i',
+      time_24hr: true
+    }
   }
-  event.fpOptionsEndDate = {
-    allowInput: false,
-    enableTime: true,
-    dateFormat: 'd/m/y H:i',
-    time_24hr: true
+
+
+  // When an event comes from the server its products cant be bound using Vue-js' v-bind to
+  // the full list of farm proposed products because they have different fields (different level of detail)
+  // This function converts the "event" products into "farm_products" by matching and replacing them
+  // it also adds the necessary configuration fields needed by the DatePicker for more info see setupDatePickerOption
+  // function
+  function adaptEvent(event, farm_products){
+    api.convertEventProdToFullProducts(event, farm_products)
+    setupDatePickerOption(event)
+    event.being_edited = false
   }
-}
 
+  export default {
+    /* global localStorage:true */
 
-// When an event comes from the server its products cant be bound using Vue-js' v-bind to
-// the full list of farm proposed products because they have different fields (different level of detail)
-// This function converts the "event" products into "farm_products" by matching and replacing them
-// it also adds the necessary configuration fields needed by the DatePicker for more info see setupDatePickerOption
-// function
-function adaptEvent(event, farm_products){
-  api.convertEventProdToFullProducts(event, farm_products)
-  setupDatePickerOption(event)
-  event.being_edited = false
-}
+    components: {
+      Dropdown
+    },
 
-export default {
-  /* global localStorage:true */
-
-  components: {
-    Dropdown
-  },
-
-  data () {
-    // get user farm id
-    this.$http.get(settings.urls.USER_INFO_URL, {
-      headers: {
-        Authorization: localStorage.getItem('id_token')
-      }
-    }).then(function successCallback (user_info) {
-      // save farm Id
-      farm_id = user_info.body['farm']['id']
-      // Get products proposed by farm
-      this.$http.get(settings.urls.FARMS_URL + farm_id, {
+    data () {
+      // get user farm id
+      this.$http.get(settings.urls.USER_INFO_URL, {
         headers: {
           Authorization: localStorage.getItem('id_token')
         }
-      }).then(function successCallback (prod_response) {
-        this.farm_products = prod_response.body['products']
-        // get events associated with this farm
-        this.$http.get(settings.urls.EVENTS_URL + '?farm_id=' + farm_id + '&embedded=1').then(function successCallback (farm_events) {
-          let received_events = farm_events.body
-          let prods = this.farm_products
-          received_events.forEach(function (event) {
-            adaptEvent(event, prods)
+      }).then(function successCallback (user_info) {
+        // save farm Id
+        farm_id = user_info.body['farm']['id']
+        // Get products proposed by farm
+        this.$http.get(settings.urls.FARMS_URL + farm_id, {
+          headers: {
+            Authorization: localStorage.getItem('id_token')
+          }
+        }).then(function successCallback (prod_response) {
+          this.farm_products = prod_response.body['products']
+          // get events associated with this farm
+          this.$http.get(settings.urls.EVENTS_URL + '?farm_id=' + farm_id + '&embedded=1').then(function successCallback (farm_events) {
+            let received_events = farm_events.body
+            let prods = this.farm_products
+            received_events.forEach(function (event) {
+              adaptEvent(event, prods)
+            })
+            this.events = received_events.reverse()
+          }, function errorCallback (error_getting_farm_events) {
+            console.log("Error getting farm events: " + error_getting_farm_events)
           })
-          this.events = received_events.reverse()
-        }, function errorCallback (error_getting_farm_events) {
-          console.log("Error getting farm events: " + error_getting_farm_events)
+        }, function (error_getting_farm_products) {
+          console.log("Error getting farm products: " + error_getting_farm_products)
         })
-      }, function (error_getting_farm_products) {
-        console.log("Error getting farm products: " + error_getting_farm_products)
+      }, function (error_getting_user_info) {
+        console.log("Error getting user info: " + error_getting_user_info)
       })
-    }, function (error_getting_user_info) {
-      console.log("Error getting user info: " + error_getting_user_info)
-    })
-    return {
-      events: [],
-      farm_products,
-    }
-  },
-
-  methods: {
-    editEvent: function (event_index) {
-      // If is being edited and is not a new event, continue with PUT, else if is new do a POST
-      if (this.events[event_index].being_edited) {
-        if(!this.events[event_index].isNewEvent){
-          this.$http({
-            url: settings.urls.EVENTS_URL + this.events[event_index].id,
-            method: 'PUT',
-            headers: {
-              'Authorization': localStorage.getItem('id_token'), 'Content-Type': 'application/json'
-            },
-            body: {
-              "title": this.events[event_index].title,
-              "description": this.events[event_index].description,
-              "beginAt": moment(this.events[event_index].beginAt, "DD/MM/YY HH:mm").unix(),
-              "endAt": moment(this.events[event_index].endAt, "DD/MM/YY HH:mm").unix(),
-              "products": api.getProductsID(this.events[event_index].products)
-            }
-          }).then(function (success_resp) {
-            // TODO: put a visual sign that it was successful
-          },
-          function (error_resp) {
-            console.log("Error editing event")
-            console.log(error_resp)
-            // TODO: put a visual sign that it was NOT successful
-          })
-        }else {
-          this.postEvent(event_index)
-        }
+      return {
+        events: [],
+        farm_products,
       }
-      // Replaces element in array, splice syntax = (begin, how many to delete, what to insert)
-      // Needs to be done like this so Vue can detect changes and update the DOM
-      this.events[event_index].being_edited = !this.events[event_index].being_edited
     },
-    postEvent: function (event_index) {
-      this.$http({url: settings.urls.EVENTS_URL, method: 'POST',
+
+    methods: {
+      editEvent: function (event_index) {
+        // If is being edited and is not a new event, continue with PUT, else if is new do a POST
+        if (this.events[event_index].being_edited) {
+          if(!this.events[event_index].isNewEvent){
+            this.$http({
+              url: settings.urls.EVENTS_URL + this.events[event_index].id,
+              method: 'PUT',
+              headers: {
+                'Authorization': localStorage.getItem('id_token'), 'Content-Type': 'application/json'
+              },
+              body: {
+                "title": this.events[event_index].title,
+                "description": this.events[event_index].description,
+                "beginAt": moment(this.events[event_index].beginAt, "DD/MM/YY HH:mm").unix(),
+                "endAt": moment(this.events[event_index].endAt, "DD/MM/YY HH:mm").unix(),
+                "products": api.getProductsID(this.events[event_index].products)
+              }
+            }).then(function (success_resp) {
+              // TODO: put a visual sign that it was successful
+            },
+            function (error_resp) {
+              console.log("Error editing event")
+              console.log(error_resp)
+              // TODO: put a visual sign that it was NOT successful
+            })
+          }else {
+            this.postEvent(event_index)
+          }
+        }
+        // Replaces element in array, splice syntax = (begin, how many to delete, what to insert)
+        // Needs to be done like this so Vue can detect changes and update the DOM
+        this.events[event_index].being_edited = !this.events[event_index].being_edited
+      },
+      postEvent: function (event_index) {
+        this.$http({url: settings.urls.EVENTS_URL, method: 'POST',
         headers: {
           'Authorization': localStorage.getItem('id_token'), 'Content-Type': 'application/json'
         }, body: {
@@ -265,13 +265,13 @@ export default {
       headers: {
         'Authorization': localStorage.getItem('id_token'), 'Content-Type': 'application/json'
       }
-      }).then(function (success) {
-          this.events.splice(event_index,1)
-          console.log(response)
-        },
-        function (error) {
-      })
-    }
+    }).then(function (success) {
+      this.events.splice(event_index,1)
+      console.log(response)
+    },
+    function (error) {
+    })
+  }
   }
 
 }
